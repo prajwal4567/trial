466bae5f8a5423d43f9b58a3b3f27a17784dd7d9
prajwal4567 <82152156+prajwal4567@users.noreply.github.com>
2022-02-17 17:17:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,19 @@
+var block1,block2;
 class attaraction{
-    constructor(a,b){
+    constructor(a,b,f,g){
         //a is x position of object 1.b is y position of object 1 
-        this.a;
-        this.b;
+        this.a=a;
+        this.b=b;
+       
         //f is x position of object 2.g is y position of object 2 
-        this.f;
-        this.g;
+        this.f=f;
+        this.g=g;
+        
     }
+
     attaract(){
-       this.object_1=createSprite(this.a,this.b,10,10);
        //c is x distance between them
+       this.block_1=rect(this.a,this.b,20,20);
        this.c=this.a-this.f;
        //l is y distance between them
        this.l=this.b-this.g;
@@ -18,36 +22,38 @@ class attaraction{
        //m is energy in y
        this.m=100-this.l;
        //this code is to attarct on x cordinate
-       this.object_1.x=this.object_1.x-this.d;
+       this.a=this.a-this.d;
        //this code is to attarct on y cordinate
-       this.object_1.y=this.object_1.y-this.m;
+       this.b=this.b-this.m;
+    
     }
-    repail()
+ 
+        repail()
     {
-        this.object_1=createSprite(this.f,this.g,10,10);
         //c is x distance between them
+        this.block_2=rect(this.f,this.g,20,20);
         this.c=this.a-this.f;
-        //l is y distance between them
-        this.l=this.b-this.g;
-         //d is energy in x
-        this.d=100-this.c;
-        //m is energy in y
-        this.m=100-this.l;
-        //this code is to attarct on x cordinate
-        this.object_1.x=this.object_1.x+this.d;
-        //this code is to attarct on y cordinate
-        this.object_1.y=this.object_1.y+this.m;
+       //l is y distance between them
+       this.l=this.b-this.g;
+       //d is energy in x
+       this.d=100-this.c;
+       //m is energy in y
+       this.m=100-this.l;
+       //this code is to attarct on x cordinate
+       this.a=this.a+this.d;
+       //this code is to attarct on y cordinate
+       this.b=this.b+this.m;
+     
     }
-}
 
+}
 function setup(){
     createCanvas(600,600);
-    block1= new attaraction(1,1);
-    block2= new attaraction(600,600);
+   block1= new attaraction(10,10,40,10);
+   block2= new attaraction(40,40,40,40);
 }
 function draw(){
-    background("red");
-    block1.attaract();
+    block1.repail();
     block2.repail();
     drawSprites();
 }
